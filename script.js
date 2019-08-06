fd29c9d5968352d2cc98e56f4611a052116e3e86
Maciej <maciej.zelazo@protonmail.com>
2019-08-06 19:05:58 +0200

    added new sections

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $('.owl-one').owlCarousel({
     loop: true,
-    // autoplay: true,
+    autoplay: true,
     items: 1,
     autoHeight: true,
     animateOut: 'fadeOut',
+    dots: false
+})
+
+$('.owl-two').owlCarousel({
+    loop: true,
+    autoplay: true,
+    items: 1,
+    autoHeight: true,
     dots: false
 })
